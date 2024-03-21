b09913ee835c95ade85fe797ebedeac62d544d5b
alextest-x <alexsnet01@gmail.com>
2024-03-21 01:30:29 -0600

    carrito de compras

---
+++ src/services/productService.js
@@ -0,0 +1,18 @@
+import { products } from "../data/products"
+
+
+export const getProducts = () => {
+
+    return products;
+    
+}
+
+export const calculateTotal = (items)=>{
+    return  items.reduce(
+        
+        (accumulator, item) => accumulator + item.product.price*item.quantity, 0);
+}
+
+
+
+
