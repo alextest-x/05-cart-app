d54c8443565153f3e02b13885234f3c12ce89dc0
alextest-x <alexsnet01@gmail.com>
2024-04-04 23:15:41 -0600

    se agrega la consulta al backend-springboot

--- src/components/CatalogView.jsx
+++ src/components/CatalogView.jsx
@@ -7,14 +7,60 @@ export const CatalogView = ({ handler })=> {
 
     const [products, setProducts] = useState([]);
 
+    /*
+    //para cargar la informacion
+    const [isLoading, setIsLoading] = useState(true);
+    */
+
+
+    /*
+    getProducts() recibe una promise que le mandamos del service
+    ProductService
+
+    se espera los productos (un arreglo de productos) 
+    y no la promesa de productos
+    que es asincrona o async
+    */
+
+
+    //hacemos la funcion para recibir los productos afuera de useffect
+    //await esperamos que se resulva la promesa, convertimos y obtenemos 
+    //los productos en json como un arreglo
+
+    const findAll = async () => {
+         const prods = await getProducts(); 
+         //para recibir los objetos del backend de producto en prods y lo actualizamos en 
+         //useState en products  se la pasamos con el setProducts
+         setProducts(prods); 
+         //setIsLoading(false);
+    }
+
+
     useEffect( 
         () => {
-            setProducts(getProducts());
-        }, []);
+
+          //aqui solo ponemos la funcion findAll()
+          //para que guarde los productos en el estado
+
+          findAll();
+
+
+            //se modifica para recibir los objetos productos
+            //setProducts(getProducts());
+               
+            //  const prods = await getProducts(); //usamos un await y lo ponemos en funcion afuera del useffect()
+            //  setProducts(prods); //para recibir los objetos del producto
+        
+          }, []);
 
   return (
     <>
     
+     
+
+     {
+        //isLoading && <div className="alert alert-info"> Cargando ... </div>
+     }
     <div className="row">
           {products.map(prod => (
 
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,8 +1,32 @@
+
+/*
+para leer  del backend spring
+comentamos porque va a leer la data de productos a un archivo products.js
 import { products } from "../data/products"
 
+*/
 
-export const getProducts = () => {
+export const getProducts = async () => {
 
+    /*
+   // para leer del backend spring
+   // usamos el fetch que regresa un promise 
+   // debemos recibirlos como un objecto Json
+   // usamos el awaite y el async()
+   */
+
+   /*
+    awaite
+   con el awaite se espera que se resuelva la repuesta
+    en la promesa (o PRomise<Response></Response>) y luego resultado
+    se devevuelve del objeto Response y no el objeto Promise
+   */
+
+
+   const response = await fetch('http://localhost:8080/products');
+   
+   //conviretendo la repuesta en datos json 
+   const products = await response.json();
     return products;
     
 }
